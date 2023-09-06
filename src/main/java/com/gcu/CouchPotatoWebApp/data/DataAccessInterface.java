4e654085883610c30d309ee8c94e7f3720b19455
cylon-raider <markel74@msn.com>
2023-09-05 20:26:09 -0700

    Added data services

---
+++ src/main/java/com/gcu/CouchPotatoWebApp/data/DataAccessInterface.java
@@ -0,0 +1,11 @@
+package com.gcu.CouchPotatoWebApp.data;
+
+import java.util.List;
+
+public interface DataAccessInterface <T>{
+    List<T> getAll();
+    T getById(int id);
+    boolean create(T t);
+    boolean update(T t);
+    boolean delete(T t);
+}
